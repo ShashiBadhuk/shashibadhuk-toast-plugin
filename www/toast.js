6b92ccb5aa14683137a6fb5d1a511b4b449974d0
Shashi Badhuk <Shashi1517@gmail.com>
2015-03-18 18:43:38 +0530

    Update lib

--- www/toast.js
+++ www/toast.js
@@ -3,8 +3,8 @@
 
 window.toast = function(str, callback) {
         cordova.exec(callback, function(err) {
-            callback('Nothing to echo.');
-        }, "Toast", "toast", str);
+            callback('Nothing to toast.');
+        }, "ToastPlugin", "toast", str);
     };
 
 
