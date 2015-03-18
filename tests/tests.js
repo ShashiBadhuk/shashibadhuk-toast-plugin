74e197647008071d3a71b6b626cd6699b89608f2
Shashi Badhuk <shashi1517@gmail.com>
2015-03-18 19:31:05 +0530

    Update tests.js

--- tests/tests.js
+++ tests/tests.js
@@ -1,4 +1,11 @@
- var options = ["Hello Dear","long"];
-    window.toast(options, function(response) {
-       console.log(response);
-    });
+var options = ["Sample Toast Message displayed using shashibadhuk-toast-plugin cordova plugin", "long"];  
+function successCallback(response) {
+   console.log(response);
+}
+function errorCallback(error) {
+   console.error(response);
+}
+window.plugins.toast.show(successCallback, errorCallback, options);
+						
+						
+						
